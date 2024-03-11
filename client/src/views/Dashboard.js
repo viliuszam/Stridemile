9ef29a2056d35cdd14e740ee6b82d955cedca11f
Deividas <deividas.grinius@ktu.edu>
2024-03-11 18:37:51 +0200

    Fixed pie chart going off bounds of div in dashboard

--- client/src/views/Dashboard.js
+++ client/src/views/Dashboard.js
@@ -219,7 +219,7 @@ export default () => {
             <p className='mb-3 text-xs text-gray-600'>Statistics of your completed achievements</p>
             <div className='flex my-auto place-items-center'>
               <p className='flex text-center text-gray-500 text-5xl font-bold'>
-                {((points.userPoints / points.totalPoints) * 100).toFixed(2)} <i className="fa-solid fa-percent"></i>
+                {((points.userPoints / points.totalPoints) * 100).toFixed(0)} <i className="fa-solid fa-percent"></i>
               </p>
               <PieChart
               slotProps={{ legend: { hidden: true } }}

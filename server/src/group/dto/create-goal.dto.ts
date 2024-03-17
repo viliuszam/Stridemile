6e063bc3bdd7a210bd1be4f51e4df6b811cc7b32
viliuszam <viliuskek@yahoo.com>
2024-03-17 13:41:25 +0200

    server: created basic endpoint for adding group goals

---
+++ server/src/group/dto/create-goal.dto.ts
@@ -0,0 +1,29 @@
+import { IsNotEmpty, IsString, IsOptional, IsNumber, IsDate, IsDateString } from 'class-validator';
+
+export class CreateGoalDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @IsNotEmpty()
+  @IsDateString()
+  start_date: Date;
+
+  @IsNotEmpty()
+  @IsDateString()
+  end_date: Date;
+
+  @IsOptional()
+  @IsString()
+  target_value?: string;
+
+  @IsNumber()
+  statusId?: number;
+
+  @IsNumber()
+  categoryId?: number;
+}

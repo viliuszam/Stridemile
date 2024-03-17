5785ced02d1a8af6edac4d031e7bf3b909f20a83
mokupsas <mockus.ri@gmail.com>
2024-03-17 14:36:32 +0200

    client: create animation for achievements card

--- client/src/views/Achievements.js
+++ client/src/views/Achievements.js
@@ -5,6 +5,8 @@ import '../styles/Home.css';
 import axios from 'axios';
 import { PieChart } from '@mui/x-charts/PieChart';
 import { isLoggedIn } from "../classes/Auth";
+import anime from "animejs";
+import style from '../styles/Achievements.css'
 
 export default () => {
 
@@ -67,6 +69,34 @@ export default () => {
         { id: 1, value: 100 - percentage, color: '#f9fafb' },
     ];
 
+    const getFlipEl = (event) => {
+        return event.target.getElementsByClassName('achievement-badge')
+        /*
+        console.log(event.target)
+        let el
+        if(event.target.classList.contains('achievement-badge')) return event.target
+        else if(event.target.parentElement.classList.contains('achievement-badge')) return event.target.parentElement
+        else return false;
+        */
+    }
+
+    const flip = (event) => {
+        let el = getFlipEl(event)
+
+        anime({
+            targets: el,
+            scaleX: 0,
+        });
+    }
+
+    const flipOut = (event) => {
+        let el = getFlipEl(event)
+
+        anime({
+            targets: el,
+            scaleX: 1,
+        });
+    }
   return isLoggedIn() ? (
     <div>
     <div className="container bg-white pt-12">
@@ -78,8 +108,11 @@ export default () => {
         </div>
         <div className="flex">
         <div className="w-full grid grid-cols-3 gap-4 pl-5 pr-5">
+
         {achievements.map((achievement) => (
-                    <div className="bg-white rounded-xl mt-3 text-center pt-5 pb-5 bg-gray-50 border border-solid border-green-400 hover:bg-gray-200">
+            <div className="flip-card h-80">
+                <div className="flip-card-inner h-full w-full">
+                    <div className="flip-card-font absolute h-full w-full bg-white rounded-xl mt-3 text-center pt-5 pb-5 bg-gray-50 border border-solid border-green-400 hover:bg-gray-200">
                         <div className="rounded-4xl pb-3 flex">
                             <img src="https://cdn1.iconfinder.com/data/icons/ui-roundicons/480/circle_award-512.png" alt="steps" className="rounded-4xl mx-auto" height={100} width={100}/>
                         </div>
@@ -98,7 +131,13 @@ export default () => {
                             )}
                         </p>
                     </div>
+                    <div className="flip-card-back absolute h-full w-full bg-white rounded-xl mt-3 text-center pt-5 pb-5 bg-gray-50 border border-solid border-green-400 hover:bg-gray-200">
+                        <p>Text goes here...</p>
+                    </div>
+                </div>
+            </div>
         ))}
+        
         </div>
         <div className="bg-gray-50 w-72 rounded-xl mt-3 p-4 mr-6">
             <div className="text-black text-center font-semibold pb-5">

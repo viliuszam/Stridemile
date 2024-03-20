ce5972cdee87245f9b248cbf5a9c4c96df28b292
mokupsas <mockus.ri@gmail.com>
2024-03-20 16:24:41 +0200

    client: fetch logged in user data

--- client/src/App.js
+++ client/src/App.js
@@ -20,13 +20,29 @@ import Groups from "./views/Groups";
 import GroupInvitation from "./views/GroupInvitation";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { logout } from "./classes/Auth";
+import { logout, isLoggedIn } from "./classes/Auth";
 import CreateEvent from "./views/CreateEvent";
 import EditGroup from "./views/EditGroup";
 import CreateGoal from "./views/CreateGoal";
 import CreateChallenge from "./views/CreateChallenge";
+import { useEffect } from "react";
 
 export default function App() {
+
+  useEffect(() => {
+    if(isLoggedIn()) {
+      axios.get(`http://localhost:3333/users/me`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+      })
+        .then(({ data }) => {
+          localStorage.setItem('user', JSON.stringify(data))
+          console.log(JSON.parse(localStorage.getItem('user')))
+        })
+    }
+  }, []);
+
   const navigate = useNavigate();
 
   // Handle axios errors
@@ -43,18 +59,18 @@ export default function App() {
   return (
     <Routes>
 
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/group/:id" element={<GroupPage />} />
-          <Route path="/achievements" element={<Achievements />} />
-          <Route path="/create-group" element={<CreateGroup />} />
-          <Route path="/edit-group" element={<EditGroup />} />
-          <Route path="/create-event" element={<CreateEvent />} />
-          <Route path="/create-goal" element={<CreateGoal />} />
-          <Route path="/create-challenge" element={<CreateChallenge />} />
-          <Route path="/groups" element={<Groups />} />
-        </Route>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/group/:id" element={<GroupPage />} />
+        <Route path="/achievements" element={<Achievements />} />
+        <Route path="/create-group" element={<CreateGroup />} />
+        <Route path="/edit-group" element={<EditGroup />} />
+        <Route path="/create-event" element={<CreateEvent />} />
+        <Route path="/create-goal" element={<CreateGoal />} />
+        <Route path="/create-challenge" element={<CreateChallenge />} />
+        <Route path="/groups" element={<Groups />} />
+      </Route>
 
       <Route element={<AuthLayout />}>
         <Route path="/password-recovery" element={<PasswordRecovery />} />

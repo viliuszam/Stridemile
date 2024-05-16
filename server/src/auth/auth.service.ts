a208676f6ea3c97365a402c15d7eca26f21cb0a3
Deividas <deividas.grinius@ktu.edu>
2024-05-16 17:47:08 +0300

    Back end validation for signing up

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { ForbiddenException,NotFoundException, Injectable } from "@nestjs/common";
+import { ForbiddenException, NotFoundException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto, AuthlogDto, AuthForgDto, AuthpassDto } from "./dto";
 import * as argon from 'argon2'
@@ -9,169 +9,180 @@ import { MailService } from './mail/mail.service';
 import { Multer } from 'multer';
 
 @Injectable()
-export class AuthService{
-    constructor(private prisma: PrismaService, private jwt: JwtService, private config: ConfigService, private mailService: MailService,) {
+export class AuthService {
+  constructor(private prisma: PrismaService, private jwt: JwtService, private config: ConfigService, private mailService: MailService,) {
 
-    }
+  }
 
 
-    async signin(dto: AuthlogDto){
-        const user = await this.prisma.user.findUnique({
-            where: {
-                username: dto.username
-            }
-        })
-
-        if (!user){
-            throw new ForbiddenException('Incorrect credentials')
-        }
-
-        const pwMatches = await argon.verify(user.hash, dto.password)
-
-        if (!pwMatches){
-            throw new ForbiddenException('Incorrect credentials')
-        }
-        return this.signToken(user.id, user.email)
-    }
-
-    async signup(dto: AuthDto, profileURL: string){
-        const hash = await argon.hash(dto.password);
-        try {
-            const user = await this.prisma.user.create({
-                data: {
-                    username: dto.name,
-                    email: dto.email,
-                    hash,
-                    profile_picture: profileURL
-                },
-            });
-    
-            return this.signToken(user.id, user.email)
-        }
-        catch(error){
-            if (error instanceof PrismaClientKnownRequestError && error.code === "P2002"){
-                if (error.message.includes('email')) {
-                    throw new ForbiddenException('Email is already taken');
-                } else if (error.message.includes('username')) {
-                    throw new ForbiddenException('Username is already taken');
-                }
-            }
-            throw error;
-        }
-        
-    }
-
-    async signToken(userId: number, email: string): Promise<{access_token: string}>{
-        const payload = {
-            sub: userId,
-            email
-        }
-        const secret = this.config.get('JWT_SECRET')
-
-        const token = await this.jwt.signAsync(payload, {
-            expiresIn: '60m',
-            secret: secret
-        })
-
-        return {
-            access_token: token,
-        }
-    }
-
-    async sendForgot(dto: AuthForgDto) {
-        try {
-          const user = await this.prisma.user.findUnique({
-            where: {
-              email: dto.email,
-            },
-          });
-          if (user) {
-            const token = await this.resetToken(user.id, user.email);
-            await this.prisma.user.update({
-              where: { id: user.id },
-              data: { resetPassToken: token,
-                      isResetValid: true },
-            });
-            await this.mailService.sendForgotPass(user, await token);
-            return true;
-          }else{
-            throw new ForbiddenException('Email does not exist');
-          }
-          
-        } catch (e) {
-          throw e;
-        }
-    }
-
-    async passReset(dto: AuthpassDto, email) {
-      const hash = await argon.hash(dto.newPassword);
-
-      const user = await this.prisma.user.findFirst({
-        where: {
-          email: email
-        }
-      })
-
-      if(!user.isResetValid){
-        throw new ForbiddenException('Password could not be updated')
+  async signin(dto: AuthlogDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        username: dto.username
       }
+    })
 
-      await this.prisma.user.update({
-        where: {
-          email: email,
-        },
+    if (!user) {
+      throw new ForbiddenException('Incorrect credentials')
+    }
+
+    const pwMatches = await argon.verify(user.hash, dto.password)
+
+    if (!pwMatches) {
+      throw new ForbiddenException('Incorrect credentials')
+    }
+    return this.signToken(user.id, user.email)
+  }
+
+  async signup(dto: AuthDto, profileURL: string) {
+
+    if (dto.password.length < 5) {
+      throw new ForbiddenException('Password must be atlest 5 characters long');
+    }
+
+    if (!dto.email.match(/^\S+@\S+\.\S+$/)) {
+      throw new ForbiddenException("Invalid email provided");
+    }
+
+    const hash = await argon.hash(dto.password);
+    try {
+      const user = await this.prisma.user.create({
         data: {
-          hash: hash,
-          resetPassToken: "",
-          isResetValid: false
+          username: dto.name,
+          email: dto.email,
+          hash,
+          profile_picture: profileURL
         },
       });
+
+      return this.signToken(user.id, user.email)
+    }
+    catch (error) {
+      if (error instanceof PrismaClientKnownRequestError && error.code === "P2002") {
+        if (error.message.includes('email')) {
+          throw new ForbiddenException('Email is already taken');
+        } else if (error.message.includes('username')) {
+          throw new ForbiddenException('Username is already taken');
+        }
+      }
+      throw error;
     }
 
-    async passChange(oldPassword: string, newPassword: string, email: string) {
+  }
 
+  async signToken(userId: number, email: string): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email
+    }
+    const secret = this.config.get('JWT_SECRET')
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '60m',
+      secret: secret
+    })
+
+    return {
+      access_token: token,
+    }
+  }
+
+  async sendForgot(dto: AuthForgDto) {
+    try {
       const user = await this.prisma.user.findUnique({
         where: {
-          email: email,
+          email: dto.email,
         },
       });
-  
-      if (!user) {
-        throw new NotFoundException('User not found');
+      if (user) {
+        const token = await this.resetToken(user.id, user.email);
+        await this.prisma.user.update({
+          where: { id: user.id },
+          data: {
+            resetPassToken: token,
+            isResetValid: true
+          },
+        });
+        await this.mailService.sendForgotPass(user, await token);
+        return true;
+      } else {
+        throw new ForbiddenException('Email does not exist');
       }
-  
-      const isOldPasswordValid = await argon.verify(user.hash, oldPassword);
-  
-      if (!isOldPasswordValid) {
-        throw new ForbiddenException('Old password is incorrect');
+
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  async passReset(dto: AuthpassDto, email) {
+    const hash = await argon.hash(dto.newPassword);
+
+    const user = await this.prisma.user.findFirst({
+      where: {
+        email: email
       }
-  
-      const newHash = await argon.hash(newPassword);
-  
-      await this.prisma.user.update({
-        where: {
-          id: user.id,
-        },
-        data: {
-          hash: newHash,
-          resetPassToken: '',
-          isResetValid: false,
-        },
-      });
+    })
+
+    if (!user.isResetValid) {
+      throw new ForbiddenException('Password could not be updated')
     }
 
-    async resetToken(userId: number, email: string) {
-        const payload = {
-          sub: userId,
-          email,
-        };
-        const secret = this.config.get('JWT_SECRET');
-    
-        const token = await this.jwt.signAsync(payload, {
-          expiresIn: '30m',
-          secret: secret,
-        });
-    
-        return token;
+    await this.prisma.user.update({
+      where: {
+        email: email,
+      },
+      data: {
+        hash: hash,
+        resetPassToken: "",
+        isResetValid: false
+      },
+    });
+  }
+
+  async passChange(oldPassword: string, newPassword: string, email: string) {
+
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
     }
+
+    const isOldPasswordValid = await argon.verify(user.hash, oldPassword);
+
+    if (!isOldPasswordValid) {
+      throw new ForbiddenException('Old password is incorrect');
+    }
+
+    const newHash = await argon.hash(newPassword);
+
+    await this.prisma.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        hash: newHash,
+        resetPassToken: '',
+        isResetValid: false,
+      },
+    });
+  }
+
+  async resetToken(userId: number, email: string) {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    const secret = this.config.get('JWT_SECRET');
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '30m',
+      secret: secret,
+    });
+
+    return token;
+  }
 }
--- server/src/reward/reward.service.ts
+++ server/src/reward/reward.service.ts
@@ -1,136 +1,136 @@
-import {Injectable} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from "src/prisma/prisma.service";
 @Injectable()
 export class RewardService {
 
-    constructor(private prisma: PrismaService) {
+  constructor(private prisma: PrismaService) {
 
-    }
-    
-    async UpdateRewards(userId: number, userPoints: number){
-      const colourRewards = await this.prisma.colourReward.findMany();
-      const emojiRewards = await this.prisma.emojiReward.findMany();
-      let changed = false;
-      for(const reward of colourRewards){
-        let hasReward = await this.UserHasReward(userId, reward.id)
-        if(userPoints >= reward.pointsRequired && !hasReward){
-          try {
-            await this.prisma.colourRewardsOnUsers.create({
-              data: {
-                userId: userId,
-                colourRewardId: reward.id,
-              }
-            });
-            changed = true
-          }
-          catch (error) {
-            if (error.code === 'P2002') {
+  }
 
-            } else {
-              throw error;
+  async UpdateRewards(userId: number, userPoints: number) {
+    const colourRewards = await this.prisma.colourReward.findMany();
+    const emojiRewards = await this.prisma.emojiReward.findMany();
+    let changed = false;
+    for (const reward of colourRewards) {
+      let hasReward = await this.UserHasReward(userId, reward.id)
+      if (userPoints >= reward.pointsRequired && !hasReward) {
+        try {
+          await this.prisma.colourRewardsOnUsers.create({
+            data: {
+              userId: userId,
+              colourRewardId: reward.id,
             }
+          });
+          changed = true
+        }
+        catch (error) {
+          if (error.code === 'P2002') {
+
+          } else {
+            throw error;
           }
         }
       }
-      for(const reward of emojiRewards){
-        let hasReward = await this.UserHasEmojiReward(userId, reward.id)
-        if(userPoints >= reward.pointsRequired && !hasReward){
-          try {
-            await this.prisma.emojiRewardsOnUsers.create({
-              data: {
-                userId: userId,
-                emojiRewardId: reward.id,
-              }
-            });
-            changed = true
-          }
-          catch (error) {
-            if (error.code === 'P2002') {
-
-            } else {
-              throw error;
+    }
+    for (const reward of emojiRewards) {
+      let hasReward = await this.UserHasEmojiReward(userId, reward.id)
+      if (userPoints >= reward.pointsRequired && !hasReward) {
+        try {
+          await this.prisma.emojiRewardsOnUsers.create({
+            data: {
+              userId: userId,
+              emojiRewardId: reward.id,
             }
+          });
+          changed = true
+        }
+        catch (error) {
+          if (error.code === 'P2002') {
+
+          } else {
+            throw error;
           }
         }
       }
-      return changed;
     }
+    return changed;
+  }
 
-    async ApplyCustomisation(userId: number, hexColour: string, emoji: string){
-      try {
-        const updatedUser = await this.prisma.user.update({
-          where: {
-            id: userId,
-          },
-          data: {
-            colourHex: hexColour,
-            emoji: emoji
-          },
-        });
-        return updatedUser;
-      } catch (error) {
-        throw new Error('Failed to update user hex colour.');
-      }
-
-    }
-
-    async UserHasReward(userId: number, rewardId: number){
-      const reward = await this.prisma.colourRewardsOnUsers.findUnique({
+  async ApplyCustomisation(userId: number, hexColour: string, emoji: string) {
+    try {
+      const updatedUser = await this.prisma.user.update({
         where: {
-            userId_colourRewardId: {
-                userId: userId,
-                colourRewardId: rewardId
-            }
-        }
+          id: userId,
+        },
+        data: {
+          colourHex: hexColour,
+          emoji: emoji
+        },
       });
-
-      return reward !== null;
+      return updatedUser;
+    } catch (error) {
+      throw new Error('Failed to update user hex colour.');
     }
 
-    async UserHasEmojiReward(userId: number, rewardId: number){
-      const reward = await this.prisma.emojiRewardsOnUsers.findUnique({
-        where: {
-            userId_emojiRewardId: {
-                userId: userId,
-                emojiRewardId: rewardId
-            }
-        }
-      });
+  }
 
-      return reward !== null;
-    }
-
-    async UserColours(userId: number){
-      const colours = await this.prisma.colourRewardsOnUsers.findMany({
-        where: {
+  async UserHasReward(userId: number, rewardId: number) {
+    const reward = await this.prisma.colourRewardsOnUsers.findUnique({
+      where: {
+        userId_colourRewardId: {
           userId: userId,
-        },
-        include: {
-          colourReward: true,
-        },
-      });
-  
-      return colours.map((cr) => ({
-        hex: cr.colourReward.colourHex,
-        name: cr.colourReward.colourName
-      }));
-    }
+          colourRewardId: rewardId
+        }
+      }
+    });
 
-    async UserEmojies(userId: number){
-      const emojies = await this.prisma.emojiRewardsOnUsers.findMany({
-        where: {
+    return reward !== null;
+  }
+
+  async UserHasEmojiReward(userId: number, rewardId: number) {
+    const reward = await this.prisma.emojiRewardsOnUsers.findUnique({
+      where: {
+        userId_emojiRewardId: {
           userId: userId,
-        },
-        include: {
-          emojiReward: true,
-        },
-      });
-  
-      return emojies.map((cr) => ({
-        emoji: cr.emojiReward.emoji
-      }));
-    }
-      
+          emojiRewardId: rewardId
+        }
+      }
+    });
 
-  
+    return reward !== null;
+  }
+
+  async UserColours(userId: number) {
+    const colours = await this.prisma.colourRewardsOnUsers.findMany({
+      where: {
+        userId: userId,
+      },
+      include: {
+        colourReward: true,
+      },
+    });
+
+    return colours.map((cr) => ({
+      hex: cr.colourReward.colourHex,
+      name: cr.colourReward.colourName
+    }));
+  }
+
+  async UserEmojies(userId: number) {
+    const emojies = await this.prisma.emojiRewardsOnUsers.findMany({
+      where: {
+        userId: userId,
+      },
+      include: {
+        emojiReward: true,
+      },
+    });
+
+    return emojies.map((cr) => ({
+      emoji: cr.emojiReward.emoji
+    }));
+  }
+
+
+
 }
